d6743669c73cb2793beb9800f45f86deb252b562
Vorachet Jaroensawas <vorachet@script-conductive.com>
2018-03-07 15:30:51 +0700

    init

---
+++ helloworld/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: vorachet
+ *
+ * Created on March 7, 2018, 3:26 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "Hello world!" << endl;
+    return 0;
+}
